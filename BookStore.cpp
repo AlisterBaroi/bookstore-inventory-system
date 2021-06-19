a26edb836d73320b0197177c7e19e41ffcadb6dc
Alister Animesh Baroi <44337842+AlisterBaroi@users.noreply.github.com>
2021-06-19 20:10:28 +0800

    Add files via upload

---
+++ BookStore.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "MenuClass.h"
+using namespace std;
+
+int main()
+{
+    Menus classObj;
+    cout << " Welcome to My Bookstore" << endl;
+    cout << "=========================\n\n";
+    classObj.mainMenu();
+    return 0;
+}
